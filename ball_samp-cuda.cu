b9420b04110145379442cb92f11b159ffd80d4f2
Praneeth <ppurini@openhpc-r8.cluster>
2024-06-21 16:46:46 -0400

    clearer

--- ball_samp-cuda.cu
+++ ball_samp-cuda.cu
@@ -51,11 +51,17 @@ __global__ void get_hists(double* res, double* hists, double*arr, int n, int siz
 
 
 int main(int argc, char* argv[]){
-	
-//	int dim = std::stoi(argv[1]);
-//	int dim = 3;
-	int n = 15000000;
-	for(int dim=2; dim<17; dim++){
+
+	// **Note** The following variables in the editables can be changed. Select what works best with your system.
+	// Editables {	
+	int min_dim = 1; // Min dimension - start dimension
+	int n = 1500000; // Number of points to sample for dim = min_dim
+	float d = 1.5; // Number of points to sample decreases by a factor of d for each increament in dim 
+	int max_dim = 16; // Max dimension - end dimension
+	// }
+
+
+	for(int dim=min_dim; dim<max_dim+1; dim++){
 
 	int rows = n;	
 	int cols = dim;
@@ -63,7 +69,7 @@ int main(int argc, char* argv[]){
 	size_t total_bytes = rows*sizeof(double);
 	
 	// Allocate memory for the matrix on host
-//	double* mat = (double*)malloc(total_bytes); // host
+	// double* mat = (double*)malloc(total_bytes); // host
 	double* matd; // device
 
 
@@ -73,7 +79,7 @@ int main(int argc, char* argv[]){
 
 	// copy host matrix to device (redundant! No need to because results are what we will obtain from the device. Also it is not initiated on host)
 
-//	cudaMemcpy(matd, mat, total_bytes, cudaMemcpyHostToDevice);
+	// cudaMemcpy(matd, mat, total_bytes, cudaMemcpyHostToDevice);
 
 	int blockSize, gridSize;
 
@@ -84,22 +90,22 @@ int main(int argc, char* argv[]){
 	gridSize = (int)ceil((float)(rows/blockSize));
 	// Execute the kernel
 	randgen<<<gridSize, blockSize>>>(matd, rows, cols);
-//	std::cout<<"done"<<std::endl;
+
+	// std::cout<<"done"<<std::endl;
 
 	// Copy matrix back to host;
-//	cudaMemcpy(mat, matd, total_bytes, cudaMemcpyDeviceToHost);
+	// cudaMemcpy(mat, matd, total_bytes, cudaMemcpyDeviceToHost);
 
 	// to print the initiated array	
-//	for(int i=0; i<100; i++){
-//		std::cout<<mat[i]<<" ";
-		
-//	}
+	// for(int i=0; i<100; i++){
+	//	std::cout<<mat[i]<<" ";	
+	//}
 
 	
 	int size = rows; // size of results	
-//	std::cout<<results[size-2]<<std::endl;
+	// std::cout<<results[size-2]<<std::endl;
 	
-	int total_vol = n; // total number of points present inside hypersphere
+	int total_vol = n; // total number of points present inside the hypersphere
 	
 	
 	// creating intervals
@@ -128,24 +134,18 @@ int main(int argc, char* argv[]){
 	cudaMemcpy(hists, histsd, (samp-1)*sizeof(double), cudaMemcpyDeviceToHost);
 
 
-//	for(int i=0; i<samp-1; i++){
-//		int sum = 0;
-//		for(int j=0; j<size; j++){
-//			if(results[j]>=arr[i] && results[j]<arr[i+1])
-//				sum += 1;
-//		}
-//		hists[i] = (double)sum/total_vol;
-//	}
-
 	double total_sum = 0;
-	std::cout<<"For dimension - "<<dim<<": ";
+	std::cout<<"For dimension - "<<dim<<": "<<std::endl;
+	std::cout<<"Number of points sampled = "<< total_vol<<std::endl;
+	std::cout<<"Fractional volumes = { " ;
 	for(int i=0; i<samp-1; i++){
 		total_sum += hists[i];
-		std::cout<<hists[i]<<" ";
+		std::cout<<hists[i]<<", ";
 	}
+	std::cout<<" }";
 	std::cout<<std::endl;
+	std::cout<<"Total sum of fractions = "<<total_sum<<std::endl;
 	std::cout<<std::endl;
-	std::cout<<total_sum<<std::endl;
 
 
 
@@ -159,7 +159,7 @@ int main(int argc, char* argv[]){
 
 
 
-	n /= 2;
+	n /= d;
 
 	cudaDeviceSynchronize();
 
